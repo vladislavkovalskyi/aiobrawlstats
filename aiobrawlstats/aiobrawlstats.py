af715ee940dd44bb8f8bede9edef7bff077f9ff2
vladislavkovalskyi <v.darknesssb@gmail.com>
2021-11-28 23:56:03 +0200

    Added debug mode, made model return

--- aiobrawlstats/aiobrawlstats.py
+++ aiobrawlstats/aiobrawlstats.py
@@ -1,10 +1,13 @@
 from aiobrawlstats.const.const import URL_PLAYER, URL_PLAYER_BATTLELOG
 from aiobrawlstats.http.http import request
+from aiobrawlstats.types.players.player import Player
+from aiobrawlstats.types.players.player_battlelog import BattleLog
 
 
 class Client:
-    def __init__(self, token: str):
+    def __init__(self, token: str, debug: bool = False):
         self._token = token
+        self.debug = debug
         self.headers = {
             "Accept": "application/json",
             "Authorization": "Bearer {}".format(self._token),
@@ -12,8 +15,8 @@ class Client:
 
     async def get_player(self, tag: str):
         response = await request(URL_PLAYER.format(tag=tag), headers=self.headers)
-        return response
+        return response if self.debug else Player(**response)
 
     async def get_battle(self, tag: str):
         response = await request(URL_PLAYER_BATTLELOG.format(tag=tag), headers=self.headers)
-        return response
+        return response if self.debug else BattleLog(**response)
--- tests.py
+++ tests.py
@@ -2,19 +2,14 @@ import asyncio
 from pprint import pprint
 
 from aiobrawlstats import Client
-from aiobrawlstats.types.players.player_battlelog import BattleLog
 
-TOKEN = ""
-
+TOKEN = "Token"
 client = Client(token=TOKEN)
 
 
 async def app():
-    # player_info = await client.get_player("R809PCRV")
-    # pprint(player_info)
-    player_battles_info = await client.get_battle("22LRPLGRY")
+    player_battles_info = await client.get_battle("PlayerTag")
     pprint(player_battles_info)
-    pprint(BattleLog(**player_battles_info), sort_dicts=False)
 
 
 loop = asyncio.get_event_loop()
