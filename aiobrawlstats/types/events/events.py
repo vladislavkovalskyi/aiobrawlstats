6370da495a4cfb26612021e29bc594949e18f3b4
thebakercat <pley4386@gmail.com>
2021-12-03 23:17:34 +0500

    style(events.py): black formatting

--- aiobrawlstats/types/events/events.py
+++ aiobrawlstats/types/events/events.py
@@ -16,9 +16,11 @@ class ScheduledEvent(BaseModel):
     end_time: datetime = Field(alias="endTime")
     event: ScheduledEventLocation = None
 
-    @validator('end_time', 'start_time', pre=True)
+    @validator("end_time", "start_time", pre=True)
     def check_names_not_empty(cls, v) -> datetime:
-        return datetime(int(v[0:4]), int(v[4:6]), int(v[6:8]), int(v[9:11]), int(v[11:13]), int(v[13:15]))
+        return datetime(
+            int(v[0:4]), int(v[4:6]), int(v[6:8]), int(v[9:11]), int(v[11:13]), int(v[13:15])
+        )
 
 
 class ScheduledEvents(BaseModel):
